8e40603010f6fa6f3c30273b54c8b8a5bb27d6e9
henrickyL <henrickyl1@gmail.com>
2024-10-25 14:00:38 -0300

    refactor: adjust scale menu

--- src/Assets/Scripts/UI/Menus/MainMenuUI.cs
+++ src/Assets/Scripts/UI/Menus/MainMenuUI.cs
@@ -1,8 +1,7 @@
-using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
+using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEditor;
+using UnityEngine.UI;
 
 public class MainMenuUI : MonoBehaviour, IUIManager
 {
@@ -10,6 +9,15 @@ public class MainMenuUI : MonoBehaviour, IUIManager
     [SerializeField] private TMP_Text menuTitleText;
     [SerializeField] private GameObject buttonPrefab;
     [SerializeField] private Transform body;
+
+    private int maxWidth = 500;
+    private int maxHeight = 200;
+    private int minWidth = 200;
+    private int minHeight = 100;
+    private float heightPercentage = 0.3f;
+    private float widthPercentage = 0.5f;
+
+
 
     private IUISubMenu subMenu;
     private (LocalizationFields,Button)[] buttons;
@@ -61,26 +69,59 @@ public class MainMenuUI : MonoBehaviour, IUIManager
 
     private void AdjustButtonPositions()
     {
+        //TODO: Use Factory to generate a button
         RectTransform rectTransform = body.GetComponent<RectTransform>();
-        float width = rectTransform.rect.width;
-        float height = rectTransform.rect.height;
+        //float width = rectTransform.rect.width;
+        //float height = rectTransform.rect.height;
+        float posY = rectTransform.transform.localPosition.y;
 
-        const int offsetY = 250;
-        float startY = height / 2 - offsetY / 2;
+        //float defaultWidth = buttonRect.rect.width;
+        //float defaultHeight = buttonRect.rect.height;
+
+        // Definir largura e altura proporcional com um máximo e mínimo
+        //float proportionalWidth = defaultWidth * widthPercentage;
+        //float proportionalHeight = defaultHeight * heightPercentage;
+        int offsetY = 100;//Mathf.Clamp(proportionalHeight, minHeight, maxHeight);
+        //float startY = height / 2 - offsetY / 2;
 
         for (int i = 0; i < buttons.Length; i++)
         {
             Button button = buttons[i].Item2;
             RectTransform buttonTransform = button.GetComponent<RectTransform>();
-            buttonTransform.localPosition = new Vector3(0, startY - i * offsetY, 0);
+            buttonTransform.localPosition = new Vector3(0, posY - i * offsetY, 0);
         }
     }
 
     private Button CreateButton(LocalizationFields textKey, System.Action onClickAction)
     {
         GameObject newButton = Instantiate(buttonPrefab, body);
+        RectTransform buttonRect = newButton.GetComponent<RectTransform>();
+
+        //// Ajuste os anchors do botão para centralizar
+        //buttonRect.anchorMin = new Vector2(0.5f, 0.5f);
+        //buttonRect.anchorMax = new Vector2(0.5f, 0.5f);
+        //buttonRect.pivot = new Vector2(0.5f, 0.5f);
+
+        // Tamanho padrão do prefab
+        float defaultWidth = buttonRect.rect.width;
+        float defaultHeight = buttonRect.rect.height;
+
+        // Definir largura e altura proporcional com um máximo e mínimo
+        float proportionalWidth = defaultWidth * widthPercentage;
+        float proportionalHeight = defaultHeight * heightPercentage;
+
+        buttonRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, Mathf.Clamp(proportionalWidth, minWidth, maxWidth));
+        buttonRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, Mathf.Clamp(proportionalHeight, minHeight, maxHeight));
+
+
         TMP_Text buttonText = newButton.GetComponentInChildren<TMP_Text>();
         buttonText.text = GetLocalizadValue(textKey).ToUpper();
+
+        // Modificar a fonte e tamanho
+        buttonText.fontSize = 24; // Ajuste conforme necessário
+        buttonText.enableAutoSizing = true;
+        buttonText.fontSizeMin = 18;
+        buttonText.fontSizeMax = 36;
 
         Button button = newButton.GetComponent<Button>();
         button.onClick.AddListener(() => onClickAction.Invoke());
