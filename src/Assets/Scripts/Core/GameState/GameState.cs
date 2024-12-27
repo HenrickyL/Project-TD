d38044f6785339c7bf1a9e1030bd5d934136cd4d
henrickyL <henrickyl1@gmail.com>
2024-12-27 12:30:38 -0300

    fix multi generate

--- src/Assets/Scripts/Core/GameState/GameState.cs
+++ src/Assets/Scripts/Core/GameState/GameState.cs
@@ -3,13 +3,20 @@ using UnityEngine;
 public class GameState : IGameState
 {
     private GameController gameController;
+    private static bool _onInitilize = false;
+
+    private void InitializeState() {
+        if (!_onInitilize) { 
+            gameController = GameController.Instance;
+            gameController.InitializeGame();  // Gera o mapa e configura o jogo
+            _onInitilize = true;
+        }
+    }
 
     public void Enter()
     {
         // Inicializa o controlador do jogo
-        gameController = GameController.Instance;
-        gameController.InitializeGame();  // Gera o mapa e configura o jogo
-
+        InitializeState();
         this.SetupEnter();
     }
 
