2b30e3fa9cafa42b5dcbbe9b93c970dd5b4b8e98
henrickyL <henrickyl1@gmail.com>
2025-01-01 01:36:24 -0300

    adjust CreateAssetMenu to factory

--- src/Assets/Scripts/Gameplay/Map/GameTileContentFactory.cs
+++ src/Assets/Scripts/Gameplay/Map/GameTileContentFactory.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-[CreateAssetMenu]
+[CreateAssetMenu(fileName = "GameTileContentFactory", menuName = "Factory/GameTileContentFactory")]
 public class GameTileContentFactory : ScriptableObject
 {
     private Scene _contentScene;
